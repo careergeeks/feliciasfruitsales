b13a3d795240ef59861c529c2ce118641ca76566
“Arnell <arnell@intracitygeeks.org>
2020-04-06 21:30:39 -0400

    added a for loop

--- fruitsales.js
+++ fruitsales.js
@@ -1,2 +1,6 @@
 let fruitInventory = ["Apples", "Peaches", "Pumpkins", "Endives"];
 let FruitPrices = [1, 1.5, 7, 12];
+
+for (let i = 0; i < fruitInventory.length; i++) {
+    console.log(`${fruitInventory[i]}`);
+}
